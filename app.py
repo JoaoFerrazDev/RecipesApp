0efe34b0267cd4a0892edbdbc48590eb4083cdd2
DevJFerraz <jferraz2000@hotmail.com>
2024-06-18 20:53:31 +0100

    register view

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ def make_app():
 if __name__ == "__main__":
     app = make_app()
     app.static_folder = 'static'
-    app.listen(8888)
+    app.listen(5001)
     db = Database()
-    print("Starting Tornado server on http://localhost:8888")
+    print("Starting Tornado server on http://localhost:5001")
     tornado.ioloop.IOLoop.current().start()
 
 
